1434a130fe022f3453de325a97ee8a893b24bc50
Marcelo Aquino <marcelo.aquino@thunders.com.br>
2019-05-15 18:20:51 -0300

    Finishing configuration for deploy on azure

--- movies/src/MV.Angular/src/environments/environment.ts
+++ movies/src/MV.Angular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:5201/api'
+  baseURL: 'https://arctouchmoviesapi.azurewebsites.net/api'
 };
 
 /*
--- movies/src/MV.WebApi/Program.cs
+++ movies/src/MV.WebApi/Program.cs
@@ -12,13 +12,22 @@ using Serilog;
 
 namespace MV.WebApi
 {
-    public static class Program
+  public static class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            
-                
-                    var host = WebHost.CreateDefaultBuilder(webHostArgs)
+      var builder = new ConfigurationBuilder();
+      builder.AddCommandLine(args);
+
+      var config = builder.Build();
+
+      var isService = !(Debugger.IsAttached || args.Contains("--console"));
+      var pathToContentRoot = Directory.GetCurrentDirectory();
+
+      var webHostArgs = args.Where(arg => arg != "--console").ToArray();
+
+
+      var host = WebHost.CreateDefaultBuilder(webHostArgs)
                         .UseConfiguration(config)
                         .ConfigureServices(s => s.AddAutofac())
                         .UseContentRoot(pathToContentRoot)
@@ -27,8 +36,8 @@ namespace MV.WebApi
                         .UseSerilog()
                         .Build();
 
-                    host.Run();
-                
-        }
+      host.Run();
+
     }
+  }
 }
