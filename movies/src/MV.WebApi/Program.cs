8c2490eb83811c962bbc320072538fe28e14ebbc
de Aquino Junior <moji@gft.com>
2019-05-15 16:58:15 -0300

    Changing program.cs in order to deploy on azure

--- movies/src/MV.WebApi/Program.cs
+++ movies/src/MV.WebApi/Program.cs
@@ -16,51 +16,19 @@ namespace MV.WebApi
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                var builder = new ConfigurationBuilder();
-                builder.AddCommandLine(args);
-
-                var config = builder.Build();
-
-                var isService = !(Debugger.IsAttached || args.Contains("--console"));
-                var pathToContentRoot = Directory.GetCurrentDirectory();
-
-                var webHostArgs = args.Where(arg => arg != "--console").ToArray();
-
-                if (isService)
-                {
-                    var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-                    pathToContentRoot = Path.GetDirectoryName(pathToExe);
-
-                    var host = WebHost.CreateDefaultBuilder(webHostArgs)
-                        .UseKestrel()
-                        .UseConfiguration(config)
-                        .ConfigureServices(s => s.AddAutofac())
-                        .UseContentRoot(pathToContentRoot)
-                        .UseStartup<Startup>()
-                        .UseSerilog()
-                        .Build();
-
-                    host.RunAsService();
-                }
-                else
-                {
+            
+                
                     var host = WebHost.CreateDefaultBuilder(webHostArgs)
                         .UseConfiguration(config)
                         .ConfigureServices(s => s.AddAutofac())
                         .UseContentRoot(pathToContentRoot)
                         .UseStartup<Startup>()
+                        .UseIISIntegration()
                         .UseSerilog()
                         .Build();
 
                     host.Run();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex}");
-            }
+                
         }
     }
 }
